445d40cf5ed7f167cd71482c335d2827be5a7746
ricky <runqian0813@163.com>
2022-11-13 20:43:36 +0800

    delete /start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker exec -it go-mall_golang_1 bash -c './nivin install && ./nivin start rpc user && ./nivin start api user'
-
-if [[ "$?" == 0 ]]; then
-  echo "go-mall start success"
-else
-  echo "go-mall start failed"
-fi
